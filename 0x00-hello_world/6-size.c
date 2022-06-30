998364676c1a745ab816b71c0bbe985e56c4d732
Undisputedemmy <adejumoemmanuel1@gmail.com>
2022-06-30 14:47:09 -0700

    tough crack

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - Entry point 
+ * main - Entry point
  *
  * Return: Always 0 (Success)
- */
-
+*/
 int main(void)
-
 {
 	int a;
 	long int b;
@@ -21,3 +19,4 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 	return (0);
+}
