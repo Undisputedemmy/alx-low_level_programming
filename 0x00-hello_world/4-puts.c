e633cee22771c25a1200e555005a561e417d4583
Undisputedemmy <adejumoemmanuel1@gmail.com>
2022-06-30 08:44:16 -0700

    c programming

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/* 
+ * main - Entry point
+ * still learning 
+ */
+int main(void)
+{       
+    puts("Programming is like building multilingual puzzle");
+    return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#1nclude <stdio.h>
+
+/* i will tell you what mate
+ * keep trying it gets better
+ */
+int main(void)
+{
+	printf ("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
