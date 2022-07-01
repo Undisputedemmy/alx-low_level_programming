f28ba8c648da6fb9bfad6d18a553f0b3f29fdcf6
Undisputedemmy <adejumoemmanuel1@gmail.com>
2022-07-01 15:14:49 +0000

    tough tasks

---
+++ 0x01-variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success!)
+ */
+
+int main (void)
+{
+	char low;
+	low = 'a';
+	while (low <= 'z')
+	{
+		if (low != 'e' && lpw != 'q')
+		{
+			putchar(low);
+		}
+		low++;
+	}
+	putchar('\n');
+	return (0);
+}
