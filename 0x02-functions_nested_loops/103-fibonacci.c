fc0493137dd04138395444e2e3de0bae77b7c421
Undisputedemmy <adejumoemmanuel1@gmail.com>
2022-07-05 20:58:35 +0000

    sleep

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * 
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int f1, f2, f3, i, a, b;
+
+	i = 1;
+	f1 = 1;
+	f2 = 2;
+	a = 2;
+	b = 2;
+
+	while (I <= 30)
+	{
+		f3 = f1 + f2;
+		i++;
+		f1 = f2;
+		f3 = f3;
+		if (f3 % b == 0)
+		{
+			a = a + f3;
+		}
+	}
+	printf('%d/n", a);
+	return (0);
+}
