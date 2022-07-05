31e3e986e72a196d9908f534efb594ebacd204e3
Undisputedemmy <adejumoemmanuel1@gmail.com>
2022-07-05 19:56:38 +0000

    common

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,6 +24,7 @@ void times_table(void)
 				{
 					_putchar(',');
 					_putchar(' ');
+					_putchar(' ');
 				}
 			}
 			else
@@ -34,7 +35,6 @@ void times_table(void)
 				{
 					_putchar(' ');
 					_putchar(',');
-					_putchar(' ');
 				}
 			}
 		}
