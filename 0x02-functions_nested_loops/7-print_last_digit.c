edadbe33772fee507e737a00770e2bc60564c18b
Undisputedemmy <adejumoemmanuel1@gmail.com>
2022-07-05 18:53:52 +0000

    print last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,9 +3,9 @@
 
 /**
  * print_last_digit - Check main
- * @n: An integer input
+ * @r: An integer input
  * Description: This function prints the last digit of a number
- * Return: last digit of number n
+ * Return: last digit of number r
  */
 
 int print_last_digit(int r)
