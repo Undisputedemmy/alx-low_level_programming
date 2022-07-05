c296b5a4aecdcf03a94afd9199789367fcd34040
Undisputedemmy <adejumoemmanuel1@gmail.com>
2022-07-05 13:25:38 +0000

    print_alphabet; clear
    git commit -m print_alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - No entry point
+ *
+ * Return: on success Nothing.
+ */
+void print_alphabet_x10(void)
+{
+	char spell:
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+		_putchar(spell);
+	}
+	_putchar('\n');
+}
