0cbea0574ef07a6a7093137d307d56ac0f699b2c
Undisputedemmy <adejumoemmanuel1@gmail.com>
2022-07-05 20:22:43 +0000

    common

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,35 +9,40 @@
 
 void times_table(void)
 {
-	long int i, j;
+	int a, b, x, y, z;
 
-	for (i = 0; i <= 9; i++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (j = 0; j <= 9; j++)
+		for (b = 0; b <= 9; b++)
 		{
-			int ret = i * j;
-
-			if (ret <= 9)
+			x = a * b;
+			y = x / 10;
+			z = x % 10;
+			if (b > 0)
 			{
-				_putchar(ret + '0');
-				if (j < 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
+			if (x >= 10)
+			{
+			_putchar(' ');
+			_putchar(y = '0');
+			_putchar(z = '0');
+			{
+			else
+			{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar(z + '0');
+			}
+			if (b != 9)
+			{
+				_putchar(',');
 			}
 			else
 			{
-				_putchar((ret / 10) + '0');
-				_putchar((ret % 10) + '0');
-				if (j < 9)
-				{
-					_putchar(' ');
-					_putchar(',');
-				}
+				_putchar('0' + z);
+				_putchar(',');
 			}
 		}
 		_putchar('\n');
 	}
 }
+
