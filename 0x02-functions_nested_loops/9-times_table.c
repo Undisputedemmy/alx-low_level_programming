bafe90ecbad3c14c83bf50fb1c24255fa5628234
Undisputedemmy <adejumoemmanuel1@gmail.com>
2022-07-05 15:46:21 +0000

    functions and nested loops

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -24,7 +24,7 @@ void times_table(void)
 				{
 					_putchar(',');
 					_putchar(' ');
-				
+					_putchar(',');
 				}
 			}
 			else
@@ -34,7 +34,6 @@ void times_table(void)
 				if (j < 9)
 				{
 					_putchar(',');
-					_putchar(' ');
 					_putchar(' ');
 				}
 			}
